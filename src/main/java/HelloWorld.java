c67a926999f35ddad8334b659173991b7a9523f5
Richard Beemer <beemerrl98@outlook.com>
2022-08-23 22:00:29 -0500

    test commit

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -6,7 +6,7 @@ public class HelloWorld {
     public static void main(String[] args) {
 
         System.out.println(sayHello());
-
+        System.out.println("hello");
     }
 
     public static String sayHello() {
